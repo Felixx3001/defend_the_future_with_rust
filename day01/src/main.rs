75295aa0b934990072bd11d7d4461a273f408526
felix <felixx3001@gmail.com>
2023-11-21 08:47:29 -0600

    add guesss_number game

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,40 @@
+use std::{io, cmp::Ordering};
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+
+    guess_number_game();
+}
+
+fn guess_number_game() {
+    
+
+    let screat_numer = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        
+        println!("please guess a number: ");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: usize = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed number: {}", guess);
+
+        match guess.cmp(&screat_numer) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+        }
+
+    }
+
 }
