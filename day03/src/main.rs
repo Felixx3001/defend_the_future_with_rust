40919789f79f84e85b72ea8a5df79db5fed9a930
felix <felixx3001@gmail.com>
2023-11-21 09:25:18 -0600

    while practice

---
+++ day03/src/main.rs
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+fn main() {
+    
+
+    let mut n = 5;
+    while n > 0 {
+        n = n + 1;
+        if n > 10 {
+            println!("{}", n);
+            break;
+        }
+    } 
+}
