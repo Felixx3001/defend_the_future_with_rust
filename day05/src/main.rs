0d7fcd3b41ce005066fcf5c8f26afb576aa991c8
felix <felixx3001@gmail.com>
2023-11-23 05:07:31 -0600

    bubble_sort

---
+++ day05/src/main.rs
@@ -0,0 +1,36 @@
+
+
+
+
+
+fn main() {
+
+
+    let mut nums = [10,9,8,7,6,5,4,3,2,1,0,-1];
+    println!("{:?}", bubble_sort_with_flag(&mut nums));
+
+    
+
+    
+}
+
+
+/* 冒泡排序（标志优化） */
+fn bubble_sort_with_flag(nums: &mut [i32]) -> &mut [i32]{
+    // 外循环：未排序区间为 [0, i]
+    for i in (1..nums.len()).rev() {
+        let mut flag = false; // 初始化标志位
+        // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端 
+        for j in 0..i {
+            if nums[j] > nums[j + 1] {
+                // 交换 nums[j] 与 nums[j + 1]
+                let tmp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = tmp;
+                flag = true;  // 记录交换元素
+            }
+        }
+        if !flag {break};  // 此轮冒泡未交换任何元素，直接跳出
+    }
+    nums
+}
